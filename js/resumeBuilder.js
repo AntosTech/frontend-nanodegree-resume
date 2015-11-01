bedc76f1d3ab9cafde5fd7b1ebdbd49e2a39f68a
Akisha Anthony <aanthony@antostechnologies.com>
2015-11-01 15:19:02 +0100

    Added code to ResumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ var name = "Akisha D Anthony";
+ var role = "SharePoint Administrator";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+ var skills = {"CSS", "HTML", "C#", "ASP.Net"}
+ var bio = {
+	 "name" : "Akisha",
+	 "role": "SharePoint Administrator",
+	 "contact info" : "910-401-2623",
+	"welcome message" : "Welcome to my biography",
+	"skills" : skills
+ }
+  
+
+  
+  
+  $("#header").append(formattedRole);
+  $("#header").prepend(formattedName);
+  $("#header").append(bio);
